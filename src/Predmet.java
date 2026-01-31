ee81cd57abbebf59b5fd789871b27c474337ad60
Patrik Hrbek <hrbek2@spsejecna.cz>
2026-01-31 23:34:54 +0100

    naplneni tridy parametry a metodami

--- src/Predmet.java
+++ src/Predmet.java
@@ -1,4 +1,25 @@
 public class Predmet {
 
+        private int vaha;
+        private String nazev;
+        private String popis;
+        private boolean prenosny;
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public int getVaha() {
+        return vaha;
+    }
+
+    public boolean isPrenosny() {
+        return prenosny;
+    }
+
 
 }
