1b26fef7598cb0adec9815d0dd534f6c768683fe
Patrik Hrbek <hrbek2@spsejecna.cz>
2026-02-01 17:31:06 +0100

    zprovozneni prikazu poloz

--- src/PrikazPoloz.java
+++ src/PrikazPoloz.java
@@ -1,7 +1,7 @@
 public class PrikazPoloz implements Prikaz{
     @Override
     public String getNazev() {
-        return "";
+        return "poloz";
     }
 
     @Override
@@ -10,7 +10,35 @@ public class PrikazPoloz implements Prikaz{
     }
 
     @Override
-    public void proved(String[] parametry, Hra hra) {
+    public void proved(String[] prikaz, Hra hra) {
 
+        if (prikaz.length < 2){
+            System.out.println("Nemůžeš nic položit");
+            return;
+        }
+
+        String nazevPredmetu = "";
+        for (int i = 1; i < prikaz.length; i++){
+            nazevPredmetu += prikaz[i];
+            if (i < prikaz.length - 1){
+                nazevPredmetu += " ";
+            }
+        }
+
+        Mistnost aktualni = hra.getAktualnimistnost();
+
+        Predmet predmet = hra.getInv().najdiPredmet(nazevPredmetu);
+
+        if (predmet == null){
+            System.out.println("Nemůžeš nic položit");
+            return;
+        }
+
+        if (hra.getInv().odeberPredmet(predmet)){
+            aktualni.pridejPredmet(predmet);
+            System.out.println("Položil jsi: " + predmet.getNazev());
+        }else{
+            System.out.println("Inventář je prázdný");
+        }
     }
 }
