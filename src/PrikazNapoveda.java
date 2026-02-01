fbcb70b44099e88847e6136c7f08e07078c45069
Patrik Hrbek <hrbek2@spsejecna.cz>
2026-02-01 20:17:42 +0100

    zprovozneni prikazu pomoc a napoveda

--- src/PrikazNapoveda.java
+++ src/PrikazNapoveda.java
@@ -1,16 +1,26 @@
 public class PrikazNapoveda implements Prikaz{
     @Override
     public String getNazev() {
-        return "";
+        return "napoveda";
     }
 
     @Override
     public String getPopis() {
-        return "";
+        return "Zobrazí seznam všech dostupných příkazů";
     }
 
     @Override
     public void proved(String[] prikaz, Hra hra) {
-
+        System.out.println("\n=== DOSTUPNÉ PŘÍKAZY ===");
+        System.out.println("jdi <směr>       - Přesun do jiné místnosti");
+        System.out.println("vezmi <předmět>  - Vezme předmět do inventáře");
+        System.out.println("poloz <předmět>  - Položí předmět z inventáře");
+        System.out.println("batoh            - Zobrazí obsah inventáře");
+        System.out.println("prozkoumej       - Prozkoumá místnost");
+        System.out.println("pouzij <předmět> - Použije předmět");
+        System.out.println("mluv             - Promluví s postavou");
+        System.out.println("pomoc <příkaz>   - Detailní nápověda k příkazu");
+        System.out.println("konec            - Ukončí hru");
+        System.out.println("\nPro detailní info použij: pomoc <název příkazu>");
     }
 }
