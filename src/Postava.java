60b83a09732e9c90bdf46e0b5ab9cb0005b469d2
Patrik Hrbek <hrbek2@spsejecna.cz>
2026-02-02 20:16:06 +0100

    zprovozneni tridy Postava

--- src/Postava.java
+++ src/Postava.java
@@ -3,4 +3,16 @@ public class Postava {
     private String jmeno;
     private String dialog;
 
+    public Postava(String jmeno, String dialog) {
+        this.jmeno = jmeno;
+        this.dialog = dialog;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getDialog() {
+        return dialog;
+    }
 }
