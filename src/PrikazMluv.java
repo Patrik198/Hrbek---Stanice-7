e881b2bba07def9db1e59eaaddbf0aaa279affd5
Patrik Hrbek <hrbek2@spsejecna.cz>
2026-02-02 20:16:54 +0100

    zprovozneni prikazu Mluv

--- src/PrikazMluv.java
+++ src/PrikazMluv.java
@@ -6,11 +6,26 @@ public class PrikazMluv implements Prikaz{
 
     @Override
     public String getPopis() {
-        return "";
+        return "Promluv s postavou v místnosti";
     }
 
     @Override
     public void proved(String[] prikaz, Hra hra) {
+        if (prikaz.length < 2){
+            System.out.println("S kým mám mluvit?");
+            return;
+        }
 
+        String jmenoPostavy = prikaz[1];
+        Mistnost aktualni = hra.getAktualnimistnost();
+
+        Postava p = aktualni.najdiPostavu(jmenoPostavy);
+
+        if (p == null){
+            System.out.println("Tady nikdo takový není.");
+            return;
+        }
+
+        System.out.println(p.getJmeno() + ": " + p.getDialog());
     }
 }
